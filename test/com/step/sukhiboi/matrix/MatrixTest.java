96b7740e6f11a9b97dcb2aeceb21d8f6aa5ee20f
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-22 00:13:39 +0530

    added add and subtraction tests

--- test/com/step/sukhiboi/matrix/MatrixTest.java
+++ test/com/step/sukhiboi/matrix/MatrixTest.java
@@ -1,13 +1,36 @@
 package com.step.sukhiboi.matrix;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class MatrixTest {
 
   @Test
-  public void simpleTest() {
-    assertEquals(3, 3);
+  public void shouldAddTwoMatrix() {
+    int[][] values1 = { { 1, 1 }, { 1, 1 } };
+    Matrix matrix1 = Matrix.createMatrix(2, 2, values1);
+
+    int[][] values2 = { { 2, 2 }, { 2, 2 } };
+    Matrix matrix2 = Matrix.createMatrix(2, 2, values2);
+
+    int[][] expectedValues = { { 3, 3 }, { 3, 3 } };
+    Matrix expected = Matrix.createMatrix(2, 2, expectedValues);
+
+    assertTrue(matrix1.add(matrix2).equals(expected));
+  }
+
+  @Test
+  public void shouldSubtractTwoMatrix() {
+    int[][] values1 = { { 10, 10 }, { 10, 10 }, { 10, 10 } };
+    Matrix matrix1 = Matrix.createMatrix(2, 2, values1);
+
+    int[][] values2 = { { 2, 2 }, { 2, 2 }, { 2, 2 } };
+    Matrix matrix2 = Matrix.createMatrix(2, 2, values2);
+
+    int[][] expectedValues = { { 8, 8 }, { 8, 8 } };
+    Matrix expected = Matrix.createMatrix(2, 2, expectedValues);
+
+    assertTrue(matrix1.subtract(matrix2).equals(expected));
   }
 }
