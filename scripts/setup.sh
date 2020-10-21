c8ea774bebd7d39b866ac4bf29ae1238a33f6e26
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-22 00:26:06 +0530

    updated setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@ mkdir lib;
 cd lib
 
 printf "Installing hamcrest-core1.3.jar..."
-curl -Ls https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar --output hamcrest-core1.3.jar
+curl -Ls https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar --output hamcrest-core-1.3.jar
 printf "Done\n"
 
 printf "Installing junit-4.13.1..."
