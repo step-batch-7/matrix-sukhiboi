94e887a94ce2aab27e362cfa1ae640f189cc584b
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-22 11:50:09 +0530

    added tests for matrix comparison
    also using TestName rule to print each test's name

--- test/com/step/sukhiboi/matrix/MatrixTest.java
+++ test/com/step/sukhiboi/matrix/MatrixTest.java
@@ -1,13 +1,40 @@
 package com.step.sukhiboi.matrix;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
 public class MatrixTest {
+  @Rule
+  public final TestName name = new TestName();
+
+  @Test
+  public void shouldReturnTrueIfSameMatrixCompared() {
+    System.out.println(name.getMethodName());
+    int[][] values1 = { { 1, 1 }, { 1, 1 } };
+    Matrix matrix1 = Matrix.createMatrix(2, 2, values1);
+
+    assertTrue(matrix1.equals(matrix1));
+  }
+
+  @Test
+  public void shouldReturnFalseIfDifferentObjectCompared() {
+    System.out.println(name.getMethodName());
+    int[][] values1 = { { 1, 1 }, { 1, 1 } };
+    Matrix matrix1 = Matrix.createMatrix(2, 2, values1);
+
+    assertFalse(matrix1.equals(new Object()));
+    assertFalse(matrix1.equals(null));
+  }
 
   @Test
   public void shouldAddTwoMatrix() {
+    System.out.println(name.getMethodName());
+
     int[][] values1 = { { 1, 1 }, { 1, 1 } };
     Matrix matrix1 = Matrix.createMatrix(2, 2, values1);
 
@@ -22,6 +49,8 @@ public class MatrixTest {
 
   @Test
   public void shouldSubtractTwoMatrix() {
+    System.out.println(name.getMethodName());
+
     int[][] values1 = { { 10, 10 }, { 10, 10 }, { 10, 10 } };
     Matrix matrix1 = Matrix.createMatrix(2, 2, values1);
 
