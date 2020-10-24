be0c16f3ca6f1b6112b4870eba9d7b059401cc9d
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-24 16:57:04 +0530

    refactor code
    in IntelliJ

---
+++ src/com/step/sukhiboi/matrix/Matrix.java
@@ -0,0 +1,160 @@
+package com.step.sukhiboi.matrix;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Matrix {
+    private final int rows;
+    private final int columns;
+    private final int[][] values;
+
+    public Matrix(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.values = new int[rows][columns];
+    }
+
+    public static Matrix createMatrix(int rows, int columns, int[][] values) {
+        if (!isValidMatrixValues(rows, columns, values)) return null;
+        Matrix newMatrix = new Matrix(rows, columns);
+        for (int row = 0; row < rows; row++) {
+            if (columns >= 0) System.arraycopy(values[row], 0, newMatrix.values[row], 0, columns);
+        }
+        return newMatrix;
+    }
+
+    public Matrix add(Matrix anotherMatrix) {
+        Matrix newMatrix = new Matrix(this.rows, this.columns);
+        if (!this.isSizeSame(anotherMatrix)) return null;
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                newMatrix.values[i][j] = this.values[i][j] + anotherMatrix.values[i][j];
+            }
+        }
+        return newMatrix;
+    }
+
+    public Matrix subtract(Matrix anotherMatrix) {
+        Matrix newMatrix = new Matrix(this.rows, this.columns);
+        if (!this.isSizeSame(anotherMatrix)) return null;
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                newMatrix.values[i][j] = this.values[i][j] - anotherMatrix.values[i][j];
+            }
+        }
+        return newMatrix;
+    }
+
+    public Matrix multiply(int multiplier) {
+        Matrix newMatrix = new Matrix(this.rows, this.columns);
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                newMatrix.values[i][j] = this.values[i][j] * multiplier;
+            }
+        }
+        return newMatrix;
+    }
+
+    public Matrix multiply(Matrix anotherMatrix) {
+        if (this.columns != anotherMatrix.rows) return null;
+        Matrix newMatrix = new Matrix(this.rows, anotherMatrix.columns);
+        for (int rows = 0; rows < this.rows; rows++) {
+            for (int columns = 0; columns < anotherMatrix.columns; columns++) {
+                int cell = 0;
+                for (int row = 0; row < this.columns; row++) {
+                    cell += this.values[rows][row] * anotherMatrix.values[row][columns];
+                }
+                newMatrix.values[rows][columns] = cell;
+            }
+        }
+        return newMatrix;
+    }
+
+    public int determinant() {
+        if (this.rows != this.columns) return 0;
+        if (this.rows == 2) {
+            return (
+                    this.values[0][0] *
+                            this.values[1][1] -
+                            this.values[0][1] *
+                                    this.values[1][0]
+            );
+        }
+        int result = 0;
+        for (int col = 0; col < this.columns; col++) {
+            int sign = col % 2 != 0 ? -1 : 1;
+            result +=
+                    sign * this.values[0][col] * this.getSubMatrix(col).determinant();
+        }
+        return result;
+    }
+
+    private static boolean isValidMatrixValues(
+            int rows,
+            int columns,
+            int[][] values
+    ) {
+        if (values.length != rows) return false;
+        for (int row = 0; row < rows; row++) {
+            if (values[row].length != columns) return false;
+        }
+        return true;
+    }
+
+    private boolean isSizeSame(Matrix matrix) {
+        return this.rows == matrix.rows && this.columns == matrix.columns;
+    }
+
+    private Matrix getSubMatrix(int colId) {
+        Matrix subMatrix = new Matrix(this.rows - 1, this.columns - 1);
+        for (int row = 1, sRow = 0; row < this.rows; row++) {
+            for (int col = 0, sCol = 0; col < this.columns; col++) {
+                if (col == colId) {
+                    continue;
+                }
+                subMatrix.values[sRow][sCol] = this.values[row][col];
+                sCol++;
+            }
+            sRow++;
+        }
+        return subMatrix;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder matrix = new StringBuilder();
+        for (int[] row: this.values){
+            for(int cell: row){
+                matrix.append(cell).append(" ");
+            }
+            matrix.append("\n");
+        }
+
+        return "Matrix{" + "rows=" + rows + ", columns=" + columns + ", values=\n" + matrix + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Matrix)) return false;
+        Matrix matrix = (Matrix) o;
+        if (!this.isSizeSame(matrix)) return false;
+
+        for (int rows = 0; rows < this.rows; rows++) {
+            for (int column = 0; column < this.columns; column++) {
+                if (this.values[rows][column] != matrix.values[rows][column]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(rows, columns);
+        result = 31 * result + Arrays.hashCode(values);
+        return result;
+    }
+}
--- test/com/step/sukhiboi/matrix/MatrixTest.java
+++ test/com/step/sukhiboi/matrix/MatrixTest.java
@@ -1,149 +1,150 @@
 package com.step.sukhiboi.matrix;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class MatrixTest {
-  private Matrix matrix1;
-  private Matrix matrix2;
+    private Matrix matrix1;
+    private Matrix matrix2;
 
-  @Before
-  public void initializeTestFields() {
-    matrix1 = Matrix.createMatrix(2, 2, new int[][] { { 1, 1 }, { 1, 1 } });
-    matrix2 = Matrix.createMatrix(2, 2, new int[][] { { 2, 2 }, { 2, 2 } });
-  }
+    @Before
+    public void initializeTestFields() {
+        matrix1 = Matrix.createMatrix(2, 2, new int[][] { { 1, 1 }, { 1, 1 } });
+        matrix2 = Matrix.createMatrix(2, 2, new int[][] { { 2, 2 }, { 2, 2 } });
+    }
 
-  //toString
+    //toString
 
-  @Test
-  public void shouldReturnStringRepresentationOfMatrix() {
-    assertEquals(matrix1.toString(), "\nSize: 2 Rows, 2 Columns\n1 1 \n1 1 \n");
-  }
+    @Test
+    public void shouldReturnStringRepresentationOfMatrix() {
+        assertEquals("Matrix{rows=2, columns=2, values=\n1 1 \n1 1 \n}",matrix1.toString());
+    }
 
-  //equals
+    //equals
 
-  @Test
-  public void shouldReturnTrueIfSameMatrixCompared() {
-    assertEquals(matrix1, matrix1);
-  }
+    @Test
+    public void shouldReturnTrueIfSameMatrixCompared() {
+        assertEquals(matrix1, matrix1);
+    }
 
-  @Test
-  public void shouldReturnFalseIfDifferentObjectCompared() {
-    assertFalse(matrix1.equals(new Object()));
-  }
+    @Test
+    public void shouldReturnFalseIfDifferentObjectCompared() {
+        assertNotEquals(matrix1, new Object());
+    }
 
-  @Test
-  public void shouldReturnFalseIfDifferentSizedMatrixCompared() {
-    int[][] values1 = { { 1 }, { 1 } };
-    matrix1 = Matrix.createMatrix(2, 1, values1);
+    @Test
+    public void shouldReturnFalseIfDifferentSizedMatrixCompared() {
+        int[][] values1 = { { 1 }, { 1 } };
+        matrix1 = Matrix.createMatrix(2, 1, values1);
 
-    int[][] values2 = { { 2, 2 }, { 2, 2 }, { 2, 2 } };
-    matrix2 = Matrix.createMatrix(3, 2, values2);
+        int[][] values2 = { { 2, 2 }, { 2, 2 }, { 2, 2 } };
+        matrix2 = Matrix.createMatrix(3, 2, values2);
 
-    assertFalse(matrix1.equals(matrix2));
-  }
+        assertNotEquals(matrix1, matrix2);
+    }
 
-  @Test
-  public void shouldReturnFalseIfDifferentMatrixCompared() {
-    assertFalse(matrix1.equals(matrix2));
-  }
+    @Test
+    public void shouldReturnFalseIfDifferentMatrixCompared() {
+        assertNotEquals(matrix1, matrix2);
+    }
 
-  @Test
-  public void shouldReturnFalseIfNullCompared() {
-    assertFalse(matrix1.equals(null));
-  }
+    @Test
+    public void shouldReturnFalseIfNullCompared() {
+        assertNotEquals(null, matrix1);
+    }
 
-  //add
+    //add
 
-  @Test
-  public void shouldAddTwoMatrixes() {
-    int[][] expectedValues = { { 3, 3 }, { 3, 3 } };
-    Matrix expected = Matrix.createMatrix(2, 2, expectedValues);
+    @Test
+    public void shouldAddTwoMatrices() {
+        int[][] expectedValues = { { 3, 3 }, { 3, 3 } };
+        Matrix expected = Matrix.createMatrix(2, 2, expectedValues);
 
-    assertEquals(matrix1.add(matrix2), expected);
-  }
+        assertEquals(matrix1.add(matrix2), expected);
+    }
 
-  @Test
-  public void shouldReturnNullIfDifferentSizeMatrixesAdded() {
-    int[][] values1 = { { 1, 1 }, { 1, 1 }, { 1, 1 } };
-    matrix1 = Matrix.createMatrix(3, 2, values1);
+    @Test
+    public void shouldReturnNullIfDifferentSizeMatricesAdded() {
+        int[][] values1 = { { 1, 1 }, { 1, 1 }, { 1, 1 } };
+        matrix1 = Matrix.createMatrix(3, 2, values1);
 
-    assertEquals(matrix1.add(matrix2), null);
-  }
+        assertNull(matrix1.add(matrix2));
+    }
 
-  //subtract
+    //subtract
 
-  @Test
-  public void shouldSubtractTwoMatrixes() {
-    int[][] expectedValues = { { -1, -1 }, { -1, -1 } };
-    Matrix expected = Matrix.createMatrix(2, 2, expectedValues);
+    @Test
+    public void shouldSubtractTwoMatrices() {
+        int[][] expectedValues = { { -1, -1 }, { -1, -1 } };
+        Matrix expected = Matrix.createMatrix(2, 2, expectedValues);
 
-    assertEquals(matrix1.subtract(matrix2), expected);
-  }
+        assertEquals(matrix1.subtract(matrix2), expected);
+    }
 
-  @Test
-  public void shouldReturnNullIfDifferentSizeMatrixesSubtracted() {
-    int[][] values1 = { { 1, 1 }, { 1, 1 }, { 1, 1 } };
-    Matrix matrix1 = Matrix.createMatrix(3, 2, values1);
+    @Test
+    public void shouldReturnNullIfDifferentSizeMatricesSubtracted() {
+        int[][] values1 = { { 1, 1 }, { 1, 1 }, { 1, 1 } };
+        Matrix matrix1 = Matrix.createMatrix(3, 2, values1);
 
-    assertEquals(matrix1.subtract(matrix2), null);
-  }
+        assertNull(matrix1.subtract(matrix2));
+    }
 
-  // multiply
+    // multiply
 
-  @Test
-  public void shouldMultiplyTwoMatrixes() {
-    int[][] expectedValues = { { 4, 4 }, { 4, 4 } };
-    Matrix expected = Matrix.createMatrix(2, 2, expectedValues);
+    @Test
+    public void shouldMultiplyTwoMatrices() {
+        int[][] expectedValues = { { 4, 4 }, { 4, 4 } };
+        Matrix expected = Matrix.createMatrix(2, 2, expectedValues);
 
-    assertEquals(matrix1.multiply(matrix2), expected);
-  }
+        assertEquals(expected, matrix1.multiply(matrix2));
+    }
 
-  @Test
-  public void shouldMultiplyTwoMatrixesOfDifferentSizes() {
-    int[][] values1 = { { 10, 10 }, { 10, 10 }, { 10, 10 } };
-    matrix1 = Matrix.createMatrix(3, 2, values1);
+    @Test
+    public void shouldMultiplyTwoMatricesOfDifferentSizes() {
+        int[][] values1 = { { 10, 10 }, { 10, 10 }, { 10, 10 } };
+        matrix1 = Matrix.createMatrix(3, 2, values1);
 
-    int[][] values2 = { { 2, 2, 2 }, { 2, 2, 2 } };
-    matrix2 = Matrix.createMatrix(2, 3, values2);
+        int[][] values2 = { { 2, 2, 2 }, { 2, 2, 2 } };
+        matrix2 = Matrix.createMatrix(2, 3, values2);
 
-    int[][] expectedValues = { { 40, 40, 40 }, { 40, 40, 40 }, { 40, 40, 40 } };
-    Matrix expected = Matrix.createMatrix(3, 3, expectedValues);
+        int[][] expectedValues = { { 40, 40, 40 }, { 40, 40, 40 }, { 40, 40, 40 } };
+        Matrix expected = Matrix.createMatrix(3, 3, expectedValues);
 
-    assertEquals(matrix1.multiply(matrix2), expected);
-  }
+        assertEquals(matrix1.multiply(matrix2), expected);
+    }
 
-  @Test
-  public void shouldReturnNullIfMatrixesWithDifferentRowAndColumnSizeMultiplied() {
-    int[][] values1 = { { 10, 10, 10 }, { 10, 10, 10 } };
-    Matrix matrix1 = Matrix.createMatrix(2, 3, values1);
+    @Test
+    public void shouldReturnNullIfMatricesWithDifferentRowAndColumnSizeMultiplied() {
+        int[][] values1 = { { 10, 10, 10 }, { 10, 10, 10 } };
+        Matrix matrix1 = Matrix.createMatrix(2, 3, values1);
 
-    assertEquals(matrix1.multiply(matrix2), null);
-  }
+        if (matrix1 != null) {
+            assertNull(matrix1.multiply(matrix2));
+        }
+    }
 
-  //determinant
+    //determinant
 
-  @Test
-  public void shouldReturnDeterminantOfMatrix() {
-    assertEquals(matrix1.determinant(), 0);
-  }
+    @Test
+    public void shouldReturnDeterminantOfMatrix() {
+        assertEquals(matrix1.determinant(), 0);
+    }
 
-  @Test
-  public void shouldReturnDeterminantOfA3By3Matrix() {
-    int[][] values1 = { { 1, 2, 1 }, { 1, 3, 4 }, { 1, 5, 3 } };
-    Matrix matrix1 = Matrix.createMatrix(3, 3, values1);
+    @Test
+    public void shouldReturnDeterminantOfA3By3Matrix() {
+        int[][] values1 = { { 1, 2, 1 }, { 1, 3, 4 }, { 1, 5, 3 } };
+        Matrix matrix1 = Matrix.createMatrix(3, 3, values1);
 
-    assertEquals(matrix1.determinant(), -7);
-  }
+        assertEquals(matrix1.determinant(), -7);
+    }
 
-  @Test
-  public void shouldReturnZeroIfANonSquareMatrixGiven() {
-    int[][] values1 = { { 1, 2, 1 }, { 1, 3, 4 } };
-    Matrix matrix1 = Matrix.createMatrix(2, 3, values1);
+    @Test
+    public void shouldReturnZeroIfANonSquareMatrixGiven() {
+        int[][] values1 = { { 1, 2, 1 }, { 1, 3, 4 } };
+        Matrix matrix1 = Matrix.createMatrix(2, 3, values1);
 
-    assertEquals(matrix1.determinant(), 0);
-  }
+        assertEquals(matrix1.determinant(), 0);
+    }
 }
