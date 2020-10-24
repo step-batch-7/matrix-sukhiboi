162dbf2ac319842fbd04f05cc4572d53153ac6ba
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-24 17:17:49 +0530

    removed scripts

--- scripts/compile.sh
+++
@@ -1,4 +0,0 @@
-printf "Compiling...";
-rm -rf out;
-mkdir out;
-javac -d out -cp $(find lib -name '*.jar' | tr '\n' ':'). $(find . -name '*.java');printf "Done\n";
--- scripts/runTest.sh
+++
@@ -1,2 +0,0 @@
-./scripts/compile.sh
-java -cp $(find lib -name '*.jar' | tr '\n' ':').:out org.junit.runner.JUnitCore com.step.sukhiboi.matrix.MatrixTest
